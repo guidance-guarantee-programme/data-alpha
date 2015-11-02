7e5017079548435b79ae2abbf3e8a550c1def89e
Andrew Garner <andrew@andrewgarner.com>
2015-11-02 09:25:30 +0000

    Add basic user interface

---
+++ app/controllers/queries_controller.rb
@@ -0,0 +1,13 @@
+class QueriesController < ApplicationController
+  def index
+    if @query = params[:query]
+      @results = olap_connection.execute(@query)
+    end
+  end
+
+  private
+
+  def olap_connection
+    @olap ||= Rails.application.config.olap_connection
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,2 +1,5 @@
 Rails.application.routes.draw do
+  root to: 'queries#index'
+
+  mount GovukAdminTemplate::Engine, at: '/style-guide'
 end
