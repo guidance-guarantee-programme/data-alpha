7c43de77a354e71b7d9a5b4d14d7781201764d3b
Andrew Garner <andrew@andrewgarner.com>
2015-11-03 11:42:54 +0000

    Configure basic authentication

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,7 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  if ENV['AUTH_USERNAME'] && ENV['AUTH_PASSWORD']
+    http_basic_authenticate_with name: ENV['AUTH_USERNAME'], password: ENV['AUTH_PASSWORD']
+  end
 end
